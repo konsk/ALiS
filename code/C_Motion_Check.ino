71255959300cecb1f3d4cc490e0f745c16b2deed
konsk <constantinidis@sch.gr>
2019-04-09 12:07:43 +0300

    Basic Code Function

--- code/C_Motion_Check.ino
+++
@@ -1,7 +0,0 @@
-/* Συνάρτηση αισθητήρα κίνησης */
-
-void motion_check()
-{                         
-  Serial.println("  MOTION  ");                           //  Διάβασε την τιμή το αισθητήρα κίνησης 1
-  if (digitalRead(in_motionPin) == HIGH) dm_counter++;    // αν έχουμε κίνηση τότε αύξησε τον μετρητή γεγονότων   
-}
