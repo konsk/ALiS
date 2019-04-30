29b9a16a7200c6837fc1c8c482f5d815e590c2d0
konsk <44722131+konsk@users.noreply.github.com>
2019-04-30 12:01:30 +0300

    Update D_Button_Pressed.ino

--- code_files/D_Button_Pressed.ino
+++ code_files/D_Button_Pressed.ino
@@ -1,20 +1,8 @@
-/* Συνάρτηση button Interrupt (ISR) */
+/* Συνάρτηση button Interrupt (ISR) :  δεν είναι έτοιμη ακόμη
+   Καλείται όταν πατηθεί το κουμπί. Σκοπός να ενεργοποιούνται  / απενεργοποιούνται τα φώτα χειροκίνητα */
 
 void button_pressed()
 {
-  if(brightness11 > 0 || brightness10 > 0 || brightness09 > 0 || brightness06 > 0) 
-  {
-    analogWrite(ledStrip11, 0);
-    analogWrite(ledStrip10, 0);
-    analogWrite(ledStrip09, 0);
-    analogWrite(ledStrip06, 0);
-  }
-  else
-  {
-    analogWrite(ledStrip11, 255);
-    analogWrite(ledStrip10, 255);
-    analogWrite(ledStrip09, 255);
-    analogWrite(ledStrip06, 255);
-  }
+  
 }
 
